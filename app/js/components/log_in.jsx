8f0905470edf387a01a3c2a162e05dc1b674c817
Emre Surmeli <s.emre.s.8@gmail.com>
2015-06-15 14:24:44 -0700

    components bundle correctly

--- app/js/components/log_in.jsx
+++ app/js/components/log_in.jsx
@@ -5,9 +5,8 @@ var React = require('react');
 module.export = React.createClass({
   render: function() {
     return (
-      <h1>Welcome to Turtle</h1>
-
       <form>
+        <h3>Welcome to Turtle</h3>
         <input type="text" value="" name="username" placeholder="Please enter your username"/>
         <input type="password" name="password" placeholder="Please enter your password"/>
         <input class="button" type="submit" value="Sign in"/>
