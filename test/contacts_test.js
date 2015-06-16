f088b6808210571445a5934f8aeaae9b27ee9223
Michael Bowen <mikee.bowen@gmail.com>
2015-06-16 10:12:23 -0700

    make working get test

--- test/contacts_test.js
+++ test/contacts_test.js
@@ -17,7 +17,7 @@ describe('Test Contact List Requests', function () {
 
   // create test contact list
   beforeEach(function (done) {
-    var testContactList = new ContactList({listOwnerId: '557f896e8d8eec45fe3a0954', friends: {'testId': 'test friend'}});
+    var testContactList = new ContactList({listOwnerId: '557f896e8d8eec45fe3a0954', friends: {'testId1': 'test friend 1', 'testId2': 'test friend 2'}});
     testContactList.save(function (err, data) {
     if (err) {
       throw err;
@@ -37,7 +37,20 @@ describe('Test Contact List Requests', function () {
   // test for making test contact list
   it('Should create a test contact list beforeEach test', function (done) {
     expect(this.testContactList.listOwnerId.toString()).to.eql('557f896e8d8eec45fe3a0954');
+    expect(this.testContactList.friends.testId1).to.eql('test friend 1');
+    expect(Object.keys(this.testContactList.friends).length).to.eql(2);
     done();
   });
+
+  // test get request
+  it('should display users contacts on GET request', function (done) {
+    chai.request('localhost:3000')
+    .get('/api/contacts/557f896e8d8eec45fe3a0954')
+    .end(function (err, res) {
+      expect(err).to.eql(null);
+      expect(typeof(res.body)).to.eql('object');
+      done();
+    })
+  })
 
 })
