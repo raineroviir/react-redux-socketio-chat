a7a689b8368a954541e4e919de1816a624122b46
Michael Bowen <mikee.bowen@gmail.com>
2015-06-16 14:43:38 -0700

    created working friend request route

--- routes/contacts_routes.js
+++ routes/contacts_routes.js
@@ -19,15 +19,23 @@ module.exports = function (router) {
   });
 
   // create temporary route to make test contact list
-  router.post('/contacts/:ownerId', function (req, res) {
+  router.post('/contacts/request', function (req, res) {
 
     var dynSet = {$set: {}};
-    dynSet.$set["friends." + req.body._id] = req.body.friend;
-    ContactList.update({listOwnerId: req.params.ownerId}, dynSet, {safe: true, upsert: true}, function (err, data) {
+    dynSet.$set['sentRequests.' + req.body.to.user_id] = {name: req.body.to.name};
+    ContactList.update({listOwnerId: req.body.from.user_id}, dynSet, {safe: true, upsert: true}, function (err, data) {
       if (err) {
         console.log(err);
       }
-      res.json({msg: 'contact added'});
+    });
+
+    var dynSet2 = {$set: {}};
+    dynSet2.$set['receivedRequests.' + req.body.from.user_id] = {name: req.body.from.name};
+    ContactList.update({listOwnerId: req.body.to.user_id}, dynSet2, {safe: true, upsert: true}, function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      res.json({msg: 'contact request sent'});
     });
 
   });
