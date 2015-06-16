721a28bf7c552da6c63efe6dbd3a455619449f2d
Raindeezy <roviir@gmail.com>
2015-06-15 17:47:57 -0700

    deleted old login create files

--- app/app.jsx
+++ app/app.jsx
@@ -2,10 +2,8 @@
 
 var React = require('react');
 
-var createUsr = require('./js/components/create_user.jsx');
-var logIn = require('./js/components/log_in.jsx');
-
-
+var create_user = require('./js/components/create_user.jsx');
+var log_in = require('./js/components/log_in.jsx');
 var WelcomePage = require('./js/components/welcome_page.jsx');
 var Router = require('react-router');
 var Route = Router.Route;
