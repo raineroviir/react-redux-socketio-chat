d1fcad27516b7647535bfe8c82acd009847355ff
Michael Bowen <mikee.bowen@gmail.com>
2015-06-16 16:21:59 -0700

    make accept route move new contact from pending to accepted for sender and receiver

--- routes/contacts_routes.js
+++ routes/contacts_routes.js
@@ -40,6 +40,47 @@ module.exports = function (router) {
 
   });
 
+  router.delete('/contacts/requests/deny/', function (req, res) {
+
+    var dynSet = {$set: {}};
+    var key = 'friends.' + req.body.to_user_id;
+    var delobj = {};
+    delobj[key] = '';
+    ContactList.update({listOwnerId: req.params.ownerId}, {$unset: delobj} , null , function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      res.json({msg: 'contact removed'});
+    });
+
+  });
+
+  router.post('/contacts/requests/accept/', function (req, res) {
+
+    // get reqest and move to freinds
+    ContactList.find({listOwnerId: req.body.from_user_id}, function (err, data){
+      if (err) {
+        console.log(err);
+        res.status(500).json({msg: 'server failed to complete your request'});
+      }
+      console.log(data);
+      console.log(data[0].receivedRequests[req.body.to_user_id]);
+      // console.log('HHHHH', data._id);
+      var userToAdd = data[0].receivedRequests[req.body.to_user_id];
+      var dynSet = {$set:userToAdd};
+      dynSet.$set['friends.' + req.body.to_user_id] = {name: userToAdd[req.body.to_user_id]};
+      ContactList.update({listOwnerId: req.body.from_user_id}, dynSet ,null , function (err, data){
+        if (err) {
+          console.log(err);
+          res.status(500).json({msg: 'server failed to complete your request'});
+        }
+        res.status(200).json({success: true});
+      });
+    });
+    // delete requset
+  });
+
+
   router.delete('/contacts/:ownerId', function (req, res) {
 
     var dynSet = {$set: {}};
