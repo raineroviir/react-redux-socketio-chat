2f1cce155981cb01d35b2c81d69ba2dc6b8f1803
Michael Bowen <mikee.bowen@gmail.com>
2015-06-19 02:31:24 -0700

    make message and thread boxes only appear on log in

--- app/js/components/dashboard.jsx
+++ app/js/components/dashboard.jsx
@@ -4,6 +4,7 @@ var Navbar          = require('./nav_bar.jsx'           );
 var ThreadSection   = require('./ThreadSection.jsx'     );
 var ChatExampleData = require('./ChatExampleData'       );
 var ChatWebAPIUtils = require('../utils/ChatWebAPIUtils');
+var Cookies = require('cookies-js');
 
 
 ChatExampleData.init();
@@ -11,10 +12,16 @@ ChatWebAPIUtils.getAllMessages();
 
 export default class Dashboard extends React.Component {
   render() {
+    var dashboardSection;
+    if (Cookies.get('eat')) {
+      dashboardSection = <div><MessageSection />
+        <ThreadSection /></div>;
+    } else {
+      dashboardSection = <div></div>;
+    }
     return (
       <div className="dashboard">
-        <MessageSection />
-        <ThreadSection />
+      {dashboardSection}
       </div>
     );
   }
