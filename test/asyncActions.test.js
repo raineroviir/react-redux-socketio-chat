fee7c8380f418e71f2a01c09e1bb12fef7b8bcc9
Raindeezy <roviir@gmail.com>
2015-11-20 10:07:06 -0800

    remove extra file, fix test, circleci

--- test/asyncActions.test.js
+++ test/asyncActions.test.js
@@ -9,35 +9,33 @@ const middlewares = [ thunk, promiseMiddleware ];
 
 // function mockStore(getState, expectedActions, done) {
 //   if (!Array.isArray(expectedActions)) {
-//     throw new Error('expectedActions should be an array of expected actions')
+//     throw new Error('expectedActions should be an array of expected actions.')
 //   }
 //   if (typeof done !== 'undefined' && typeof done !== 'function') {
-//     throw new Error('done should either be undefined or function')
+//     throw new Error('done should either be undefined or function.')
 //   }
 //
 //   function mockStoreWithoutMiddleware() {
 //     return {
 //       getState() {
-//         return typeof getState === 'function' ? getState() : getState
+//         return typeof getState === 'function' ?
+//           getState() :
+//           getState;
 //       },
 //
 //       dispatch(action) {
 //         const expectedAction = expectedActions.shift()
-//
-//         try {
 //           expect(action).toEqual(expectedAction)
 //           if (done && !expectedActions.length) {
 //             done()
 //           }
 //           return action
-//         } catch (e) {
-//           done(e)
 //         }
 //       }
 //     }
-//   }
-//
-//   const mockStoreWithMiddleware = applyMiddleware(...middlewares)(mockStoreWithoutMiddleware)
+//   const mockStoreWithMiddleware = applyMiddleware(
+//     ...middlewares
+//   )(mockStoreWithoutMiddleware)
 //
 //   return mockStoreWithMiddleware()
 // }
@@ -47,71 +45,16 @@ const middlewares = [ thunk, promiseMiddleware ];
 //     nock.cleanAll()
 //   })
 //
-//   it('creates LOADED_MESSAGES_SUCCESS when fetching messages has been done', (done) => {
-//     nock('http://localhost:3000')
+//   it('creates LOAD_MESSAGES_SUCCESS when fetching messages has been done', (done) => {
+//     nock('http://localhost:3000/')
 //       .get('/api/messages')
-//       .reply(200, { messages: { data: [] } })
+//       .reply(200, { messages: ['do something'] })
 //
-//     const expectedActions = [{
-//       type: types.LOAD_MESSAGES
-//     },
-//     {
-//       type: types.LOAD_MESSAGES_SUCCESS
-//     }
+//     const expectedActions = [
+//       { type: types.LOAD_MESSAGES },
+//       { type: types.LOAD_MESSAGES_SUCCESS, body: { messages: ['do something']  } }
 //     ]
-//     const store = mockStore({  messages: { data: [] } }, expectedActions, done)
+//     const store = mockStore({ messages: [] }, expectedActions, done)
 //     store.dispatch(actions.fetchMessages())
 //   })
 // })
-
-function mockStore(getState, expectedActions, done) {
-  if (!Array.isArray(expectedActions)) {
-    throw new Error('expectedActions should be an array of expected actions.')
-  }
-  if (typeof done !== 'undefined' && typeof done !== 'function') {
-    throw new Error('done should either be undefined or function.')
-  }
-
-  function mockStoreWithoutMiddleware() {
-    return {
-      getState() {
-        return typeof getState === 'function' ?
-          getState() :
-          getState;
-      },
-
-      dispatch(action) {
-        const expectedAction = expectedActions.shift()
-          expect(action).toEqual(expectedAction)
-          if (done && !expectedActions.length) {
-            done()
-          }
-          return action
-        }
-      }
-    }
-  const mockStoreWithMiddleware = applyMiddleware(
-    ...middlewares
-  )(mockStoreWithoutMiddleware)
-
-  return mockStoreWithMiddleware()
-}
-
-describe('async actions', () => {
-  afterEach(() => {
-    nock.cleanAll()
-  })
-
-  it('creates LOAD_MESSAGES_SUCCESS when fetching messages has been done', (done) => {
-    nock('http://localhost:3000/')
-      .get('/api/messages')
-      .reply(200, { messages: ['do something'] })
-
-    const expectedActions = [
-      { type: types.LOAD_MESSAGES },
-      { type: types.LOAD_MESSAGES_SUCCESS, body: { messages: ['do something']  } }
-    ]
-    const store = mockStore({ messages: [] }, expectedActions, done)
-    store.dispatch(actions.fetchMessages())
-  })
-})
