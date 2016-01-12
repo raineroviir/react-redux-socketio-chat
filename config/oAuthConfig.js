c10fde5537bb9ea2df97d84551e0fd761ca39610
Rainer Oviir <roviir@gmail.com>
2016-01-11 19:49:42 -0800

    Update oAuthConfig.js

--- config/oAuthConfig.js
+++ config/oAuthConfig.js
@@ -1,7 +1,8 @@
 var ids = {
   facebook: {
-    clientID: 'get your own',
-    clientSecret: 'get your own :)'
+    clientID: '509315679243380',
+    clientSecret: '442b39ce1ee0ae99eec51fe832519cb0'
   }
 }
+// please get your own IDs. :)
 module.exports = ids;
